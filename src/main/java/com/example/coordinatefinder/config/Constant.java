0496debe844e0a6d062986f490dc9d95c5c381ca
zagnos <czagnos@gmail.com>
2024-01-06 00:51:37 +0300

    Integrate redis

---
+++ src/main/java/com/example/coordinatefinder/config/Constant.java
@@ -0,0 +1,10 @@
+package com.example.coordinatefinder.config;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constant {
+
+    public final String COORDINATE_REQUEST_CACHE_NAME = "coordinateRequest";
+    public final String DEFAULT = "default";
+}
