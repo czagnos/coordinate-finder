271d8d31aced5a04dcc7c7348007f5ee73cd4f7c
zagnos <czagnos@gmail.com>
2024-01-05 21:47:51 +0300

    Inıt commit

---
+++ src/main/java/com/example/coordinatefinder/CoordinateFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.coordinatefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinateFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoordinateFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coordinatefinder/CoordinateFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coordinatefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordinateFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
